19d4fccbec9a74beeff5e1d70067262807fea4da
Robert Phillips <rpdecks@gmail.com>
2020-03-19 02:51:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[counter][:cost] / coupons[counter][:num],
-          :clearance => cart_item_with_coupon[counter][:clearance],
+          :clearance => cart_item[counter][:clearance],
           :count => coupons[counter][:num]
         }
         cart << cart_item_with_coupon
