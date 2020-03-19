d2533197c0f606c1276d528c92d5c6ac5455d8b6
Robert Phillips <rpdecks@gmail.com>
2020-03-19 03:15:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -102,6 +102,6 @@ def checkout(cart, coupons)
   
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
-  
+  total = apply_clearance(couponed_cart)
   
 end
